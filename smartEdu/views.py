c2eeb55d1427eec6520225f9c12f72860f30d2ad
UzbekDev2024 <157269376+UzbekDev2024@users.noreply.github.com>
2026-05-02 13:37:56 +0500

    superadmin return

--- smartEdu/views.py
+++ smartEdu/views.py
@@ -8,13 +8,13 @@ from django.views.decorators.csrf import csrf_exempt
 def github_webhook(request):
     if request.method == "POST":
         # Loyiha papkasiga o'tish (username va loyiha nomini o'zingiznikiga o'zgartiring)
-        repo_dir = '/home/Hacker99000/talim'
+        repo_dir = '/home/SmartTalim/Smart-Talim-BackEnd/'
         # https: // www.pythonanywhere.com / user / Hacker99000 /
         # GitHub'dan yangi kodni tortish (pull)
         subprocess.run(['git', 'pull', 'origin', 'main'], cwd=repo_dir)
 
         # PythonAnywhere serverini avtomatik qayta ishga tushirish (reload)
-        wsgi_file = '/var/www/Hacker99000_pythonanywhere_com_wsgi.py'
+        wsgi_file = '/var/www/SmartTalim_pythonanywhere_com_wsgi.py'
         subprocess.run(['touch', wsgi_file])
 
         return HttpResponse("Server muvaffaqiyatli yangilandi!", status=200)
